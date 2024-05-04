7800e24a1ddfdce8ba524ef3f9a4d414aefef31f
Krislv4nk <kristinalev4enko89@gmail.com>
2024-05-04 04:14:00 +0200

    Catalog list

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+// import { selectCampers } from "../../redux/camperSlice.selectors";
+// import {  useSelector } from "react-redux";
 
 
-export const CatalogList = () => {
+export const CatalogList = ({ _id }) => {
+//     const campers = useSelector(selectCampers);
+// console.log(campers);
     return (
         <div>
-            <h1>Catalog</h1>
+            <ul>
+                {/* {campers &&campers.map(({ _id, name, price, rating,
+                    location, adults, children, engine,
+                    transmission, form, length, width, height,
+                    tank, consumption, description, details,
+                    gallery, reviews, error, isLoading, favorites }) => (
+                    <li key={_id}>
+                        <img src="" alt="" />
+                        <h3>{name}</h3>
+                        <p>{price}</p>
+                        <p>{rating}</p>
+                        <p>{location}</p>
+                        <p>{adults}</p>
+                        <p>{children}</p>
+                        <p>{engine}</p>
+                        <p>{transmission}</p>
+                        <p>{form}</p>
+                        <p>{length}</p>
+                        <p>{width}</p>
+                        <p>{height}</p>
+                        <p>{tank}</p>
+                        <p>{consumption}</p>
+                        <p>{description}</p>
+                        <p>{details}</p>
+                        <p>{gallery}</p>
+                        <p>{reviews}</p>
+                        <p>{error}</p>
+                        <p>{isLoading}</p>
+                        <p>{favorites}</p>
+                    </li> */}
+
+                   {/* ))} */}
+            </ul>
         </div>
-    );
+    )
 }
